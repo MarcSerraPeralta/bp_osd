538c9f90af38e55c9e973deeed07e153aac69c39
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-11-14 15:29:49 +0100

    Solve bugs in test

--- tests/test_bp_osd.py
+++ tests/test_bp_osd.py
@@ -4,24 +4,29 @@ import numpy as np
 from pymatching import Matching
 from bp_osd import BP_OSD
 
-circuit = stim.Circuit.generated(
-    code_task="surface_code:rotated_memory_z",
-    distance=3,
-    rounds=5,
-    after_clifford_depolarization=0.01,
-)
 
+def test_bp_osd_performance():
+    circuit = stim.Circuit.generated(
+        code_task="surface_code:rotated_memory_z",
+        distance=3,
+        rounds=3,
+        after_clifford_depolarization=0.01,
+    )
 
-sampler = circuit.compile_detector_sampler()
-defects, log_flips = sampler.sample(shots=100_000, separate_observables=True)
+    sampler = circuit.compile_detector_sampler()
+    defects, log_flips = sampler.sample(shots=100_000, separate_observables=True)
 
-mwpm = Matching(circuit.detector_error_model(decompose_errors=True))
-predictions = mwpm.decode_batch(defects)
-log_prob_mwpm = np.average(predictions != log_flips)
+    mwpm = Matching(circuit.detector_error_model(decompose_errors=True))
+    predictions = mwpm.decode_batch(defects)
+    log_prob_mwpm = np.average(predictions != log_flips)
 
-bp_osd_cs = BP_OSD(circuit.detector_error_model(), osd_method="osd_cs", osd_order=40)
-predictions = bp_osd_cs.decode_batch(defects)
-print(np.average(predictions != log_flips))
-log_prob_bposd = np.average(predictions != log_flips)
+    bp_osd_cs = BP_OSD(
+        circuit.detector_error_model(), osd_method="osd_cs", osd_order=40
+    )
+    predictions = bp_osd_cs.decode_batch(defects)
+    print(np.average(predictions != log_flips))
+    log_prob_bposd = np.average(predictions != log_flips)
 
-assert log_prob_bposd < log_prob_mwpm
+    assert log_prob_bposd < log_prob_mwpm
+
+    return
